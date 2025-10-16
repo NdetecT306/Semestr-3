667c8e8c9cd635e3231b1e6bbc0c5e1f7e9c24fc
NdetecT306 <nstasya777@mail.ru>
2025-10-16 17:19:59 +0700

    Create screen.h

---
+++ Laba1/screen.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <map>
+#include "head.h"
+using namespace std;
+void saveToFile(const string& filename);
+void loadFromFile(const string& filename);
+string processQuery(const string& query, const string& filename);
