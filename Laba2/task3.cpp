e826289a90a5c4efd0df6273bfe67a856bf0c385
NdetecT306 <nstasya777@mail.ru>
2025-10-16 10:05:12 +0700

    Create task3.cpp

---
+++ Laba2/task3.cpp
@@ -0,0 +1,224 @@
+#include <iostream>
+#include <string>
+using namespace std;
+const int HASH_SIZE = 256; 
+struct DOUBLE_HASH {
+    int key;        
+    int value;      
+    bool isDeleted;
+    bool isEmpty;
+};
+DOUBLE_HASH hashTable[HASH_SIZE];
+int hash1(int key) {
+    return key % HASH_SIZE;
+}
+int hash2(int key) {
+    return 13 - (key % 13); 
+}
+void CREATE() {
+    for (int i = 0; i < HASH_SIZE; i++) {
+        hashTable[i].key = -1;
+        hashTable[i].value = -1;
+        hashTable[i].isDeleted = false;
+        hashTable[i].isEmpty = true;
+    }
+}
+bool ADD(int key, int value) {
+    int index = hash1(key);
+    int step = hash2(key);
+    int startIndex = index;
+    int attempts = 0;
+    while (attempts < HASH_SIZE) {
+        if (hashTable[index].isEmpty || hashTable[index].isDeleted) {
+            hashTable[index].key = key;
+            hashTable[index].value = value;
+            hashTable[index].isDeleted = false;
+            hashTable[index].isEmpty = false;
+            return true;
+        }
+        if (hashTable[index].key == key && !hashTable[index].isDeleted) {
+            hashTable[index].value = value;
+            return true;
+        }
+        index = (index + step) % HASH_SIZE;
+        attempts++;
+        if (index == startIndex) {
+            break;
+        }
+    }
+    return false;
+}
+bool POISK(int key, int& result) {
+    int index = hash1(key);
+    int step = hash2(key);
+    int startIndex = index;
+    int attempts = 0;
+    while (attempts < HASH_SIZE) {
+        if (hashTable[index].isEmpty && !hashTable[index].isDeleted) {
+            return false;
+        }
+        if (hashTable[index].key == key && !hashTable[index].isDeleted) {
+            result = hashTable[index].value;
+            return true;
+        }
+        index = (index + step) % HASH_SIZE;
+        attempts++;
+        if (index == startIndex) {
+            break;
+        }
+    }
+    return false;
+}
+bool DELETE(int key) {
+    int index = hash1(key);
+    int step = hash2(key);
+    int startIndex = index;
+    int attempts = 0;
+    while (attempts < HASH_SIZE) {
+        if (hashTable[index].isEmpty && !hashTable[index].isDeleted) {
+            return false;
+        }
+        if (hashTable[index].key == key && !hashTable[index].isDeleted) {
+            hashTable[index].isDeleted = true;
+            hashTable[index].isEmpty = true;
+            return true;
+        }
+        index = (index + step) % HASH_SIZE;
+        attempts++;
+        if (index == startIndex) {
+            break;
+        }
+    }
+    return false;
+}
+void getAllElements(int* keys, int* values, int& count) {
+    count = 0;
+    for (int i = 0; i < HASH_SIZE; i++) {
+        if (!hashTable[i].isEmpty && !hashTable[i].isDeleted) {
+            keys[count] = hashTable[i].key;
+            values[count] = hashTable[i].value;
+            count++;
+        }
+    }
+}
+int calculateTotalSum() {
+    int sum = 0;
+    for (int i = 0; i < HASH_SIZE; i++) {
+        if (!hashTable[i].isEmpty && !hashTable[i].isDeleted) {
+            sum += hashTable[i].key;
+        }
+    }
+    return sum;
+}
+int findMinDifference(int* keys, int count, int totalSum) {
+    bool dp[HASH_SIZE][HASH_SIZE * 100] = {false}; 
+    for (int i = 0; i <= count; i++) {
+        dp[i][0] = true;
+    }
+    for (int i = 1; i <= count; i++) {
+        for (int j = 0; j <= totalSum; j++) {
+            dp[i][j] = dp[i - 1][j];
+            if (keys[i - 1] <= j) {
+                dp[i][j] = dp[i][j] || dp[i - 1][j - keys[i - 1]];
+            }
+        }
+    }
+    int minRaz = totalSum;
+    for (int j = totalSum / 2; j >= 0; j--) {
+        if (dp[count][j]) {
+            minRaz = totalSum - 2 * j;
+            break;
+        }
+    }
+    
+    return minRaz;
+}
+void findSubsets(int* keys, int count, int minDiff, int totalSum, int* subset1, int& count1, int* subset2, int& count2) {
+    int target = (totalSum - minDiff) / 2;
+    bool dp[HASH_SIZE][HASH_SIZE * 100] = {false};
+    for (int i = 0; i <= count; i++) {
+        dp[i][0] = true;
+    }
+    for (int i = 1; i <= count; i++) {
+        for (int j = 0; j <= target; j++) {
+            dp[i][j] = dp[i - 1][j];
+            if (keys[i - 1] <= j) {
+                dp[i][j] = dp[i][j] || dp[i - 1][j - keys[i - 1]];
+            }
+        }
+    }
+    bool inSubset1[HASH_SIZE] = {false};
+    int i = count, j = target;
+    while (i > 0 && j > 0) {
+        if (dp[i][j] && !dp[i - 1][j]) {
+            inSubset1[i - 1] = true;
+            j -= keys[i - 1];
+        }
+        i--;
+    }
+    count1 = 0;
+    count2 = 0;
+    for (int k = 0; k < count; k++) {
+        if (inSubset1[k]) {
+            subset1[count1++] = keys[k];
+        } else {
+            subset2[count2++] = keys[k];
+        }
+    }
+}
+int calculateArraySum(int* arr, int count) {
+    int sum = 0;
+    for (int i = 0; i < count; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+void partitionSet() {
+    int keys[HASH_SIZE], values[HASH_SIZE];
+    int count = 0;
+    getAllElements(keys, values, count);
+    if (count == 0) {
+        cout << "Множество пусто!" << endl;
+        return;
+    }
+    cout << "Исходное множество: {";
+    for (int i = 0; i < count; i++) {
+        cout << keys[i];
+        if (i < count - 1) cout << ", ";
+    }
+    cout << "}" << endl;
+    int totalSum = calculateTotalSum();
+    int minDiff = findMinDifference(keys, count, totalSum);
+    int subset1[HASH_SIZE], subset2[HASH_SIZE];
+    int count1 = 0, count2 = 0;
+    findSubsets(keys, count, minDiff, totalSum, subset1, count1, subset2, count2);
+    cout << "Подмножество 1: {";
+    for (int i = 0; i < count1; i++) {
+        cout << subset1[i];
+        if (i < count1 - 1) cout << ", ";
+    }
+    cout << "} (сумма = " << calculateArraySum(subset1, count1) << ")" << endl;
+    cout << "Подмножество 2: {";
+    for (int i = 0; i < count2; i++) {
+        cout << subset2[i];
+        if (i < count2 - 1) cout << ", ";
+    }
+    cout << "} (сумма = " << calculateArraySum(subset2, count2) << ")" << endl;
+    cout << "Разница сумм = " << minDiff << endl;
+}
+int main() {
+    setlocale(LC_ALL, "rus");
+    CREATE();
+    int n;
+    cout << "Введите количество чисел для добавления: ";
+    cin >> n;
+    cout << "Введите " << n << " чисел:" << endl;
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        ADD(num, num); // Используем ключ и значение одинаковыми
+    }
+    cout << "\nРезультат разбиения множества:" << endl;
+    partitionSet();
+    return 0;
+}
