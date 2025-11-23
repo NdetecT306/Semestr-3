1d387ff8ede66ddaabdc618c8de33603207b7dbe
NdetecT306 <nstasya777@mail.ru>
2025-11-23 19:37:18 +0700

    Update lock.h

--- Practice1/lock.h
+++ Practice1/lock.h
@@ -1,8 +1,9 @@
+#ifndef TABLE_LOCKS_H
+#define TABLE_LOCKS_H
 #include <string>
 using namespace std;
-// Функции управления блокировками таблиц
 bool lockTable(const string& tableName);
 bool unlockTable(const string& tableName);
 bool isTableLocked(const string& tableName);
 bool waitForTableUnlock(const string& tableName, int maxWaitMs = 5000);
-
+#endif 
