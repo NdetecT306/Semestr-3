6e0e608b87a3b7e2510d4e39bcbd0576f2b7a58a
NdetecT306 <nstasya777@mail.ru>
2025-11-27 01:00:18 +0700

    Create screen.h

---
+++ Laba3/screen.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <map>
+#include "array.h"
+#include "hash.h"
+#include "dwospis.h"
+#include "odspis.h"
+#include "stack.h"
+#include "queue.h"
+#include "tree.h"
+#include "hash.h"
+using namespace std;
+void saveToFile(const string& filename);
+void loadFromFile(const string& filename);
+string processQuery(const string& query, const string& filename);
