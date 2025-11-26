aeab59b6a7a115890ac67e64e15bc2a0457a3de4
NdetecT306 <nstasya777@mail.ru>
2025-11-27 00:49:58 +0700

    Update queue.h

--- Laba3/queue.h
+++ Laba3/queue.h
@@ -1,6 +1,7 @@
 #include <iostream>
-#include<string>
+#include <string>
 using namespace std;
+
 class Queue {
 private:
     string toy;
@@ -8,10 +9,17 @@ private:
 public:
     Queue() : toy(""), site(nullptr) {}
     Queue(string str) : toy(str), site(nullptr) {}
+    
     static void QPUSH(Queue*& head, Queue*& tail, string str);
-    static void QDELETE(Queue*& ptr);
-    static string QPOP(Queue*& ptr);
+    static void QDELETE(Queue*& head, Queue*& tail);
+    static string QPOP(Queue*& head, Queue*& tail);
     static void readQueue(Queue* ptr);
     static int countQueue(Queue* ptr);
+    static void clearQueue(Queue*& head, Queue*& tail);
+    
+    // Геттеры для доступа к приватным полям
+    string getToy() const { return toy; }
+    Queue* getSite() const { return site; }
+    
     ~Queue();
 };
